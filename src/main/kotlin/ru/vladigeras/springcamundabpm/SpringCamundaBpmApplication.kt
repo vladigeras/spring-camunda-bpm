f80543295a21a6b49b082555eda3005358cfd1b4
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2021-05-29 16:06:04 +0300

    Initial commit

---
+++ src/main/kotlin/ru/vladigeras/springcamundabpm/SpringCamundaBpmApplication.kt
@@ -0,0 +1,11 @@
+package ru.vladigeras.springcamundabpm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCamundaBpmApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringCamundaBpmApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vladigeras/springcamundabpm/SpringCamundaBpmApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vladigeras.springcamundabpm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCamundaBpmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
